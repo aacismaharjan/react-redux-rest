a0fd1d867e33076a3876acdff1ae2d137fababec
Aashish Maharjan <aashishmaharjan2001@gmail.com>
2020-10-17 16:59:39 +0545

    Updates

---
+++ precache-manifest.2e47800f79c7d59cea507076f2a51598.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7e137e0c03c5ea171862ae49a7024fe2",
+    "url": "/react-redux-rest/index.html"
+  },
+  {
+    "revision": "88c2716b4a18ef39662f",
+    "url": "/react-redux-rest/static/css/main.40a27ff4.chunk.css"
+  },
+  {
+    "revision": "25ec51aec5212313c4fa",
+    "url": "/react-redux-rest/static/js/2.bf2181b0.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/react-redux-rest/static/js/2.bf2181b0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "88c2716b4a18ef39662f",
+    "url": "/react-redux-rest/static/js/main.bfa23e42.chunk.js"
+  },
+  {
+    "revision": "699271982d2e890ecfb1",
+    "url": "/react-redux-rest/static/js/runtime-main.77dca5e1.js"
+  }
+]);
